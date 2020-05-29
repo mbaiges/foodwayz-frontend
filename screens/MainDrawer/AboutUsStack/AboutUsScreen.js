d6bab7b8871e90cd4b210d5b7d15b024ee76ca7b
CamilaPonce <cponce@itba.edu.ar>
2020-05-29 18:17:17 -0300

    about and contact us

--- screens/MainDrawer/AboutUsStack/AboutUsScreen.js
+++ screens/MainDrawer/AboutUsStack/AboutUsScreen.js
@@ -1,141 +1,112 @@
 import React, { Component } from "react";
-import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
 
-function AboutUs(props) {
+import {
+  StyleSheet,
+  View,
+  SafeAreaView,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  Button,
+  Dimensions
+} from "react-native";
+
+import { Image, ListItem, Icon, Input, Rating} from 'react-native-elements';
+
+class FoodScreenComponent extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    const { navigation } = this.props;
+    const { rating } = this.props;
+
   return (
-    <View style={styles.container}>
-      <View style={styles.rectStack}>
-        <View style={styles.rect2}></View>
-      </View>
+    <SafeAreaView style={styles.backgroundContainer}>
       <View style={styles.aboutUsStackStack}>
         <View style={styles.aboutUsStack}>
-          <Text style={styles.aboutUs}>About us</Text>
-          <View style={styles.rect3}></View>
+          <Text style={styles.logoText}>About us</Text>
         </View>
-        <Text style={styles.whoAreWe}>Who are we?</Text>
+        <View>
+          <Text style={styles.subtitle}>Who are we?</Text>
+          <Text style={styles.text}>
+            We are a team of students from Instituto Tecnológico de Buenos Aires,
+            named Dychromatic. Together we have created Foodwayz.
+          </Text>
+        </View>
+        <View>
+          <Text style={styles.subtitle}>What is Foodwayz?</Text>
+          <Text style={styles.text}>
+            Foodwayz is your solution to different problems that remain unsolved
+            by other food applications. Our aim is to provide our customers with a
+            different point of view. {"\n"}
+            {"\n"}What happens when you are allergic and the ingredients are not
+            specified in the description of the product? What if you go to a
+            restaurant for the first time and don&#39;t know what to order? Are
+            you tired of always ordering the sasme food because you are afraid of
+            not getting something you like?{"\n"}
+            {"\n"}Foodwayz is the solution! As our application is focused on the
+            ingredients as well as on the product itself. Our app has the option
+            to search by plate, ingredient, restaurant, area and price. It has an
+            accurate system of reviews that, combined with the detailed
+            description of each product, gives the user the most complete dining
+            experience.
+          </Text>
+        </View>
         <TouchableOpacity style={styles.button}>
           <Text style={styles.back}>BACK</Text>
         </TouchableOpacity>
-        <Text style={styles.weAre}>
-          We are a team of students from Instituto Tecnológico de Buenos Aires,
-          named Dychromatic. Together we have created Foodwayz.
-        </Text>
-        <Text style={styles.whatIsFoodwayz}>What is Foodwayz?</Text>
-        <Text style={styles.weAre1}>
-          Foodwayz is your solution to different problems that remain unsolved
-          by other food applications. Our aim is to provide our customers with a
-          different point of view. {"\n"}
-          {"\n"}What happens when you are allergic and the ingredients are not
-          specified in the description of the product? What if you go to a
-          restaurant for the first time and don&#39;t know what to order? Are
-          you tired of always ordering the sasme food because you are afraid of
-          not getting something you like?{"\n"}
-          {"\n"}Foodwayz is the solution! As our application is focused on the
-          ingredients as well as on the product itself. Our app has the option
-          to search by plate, ingredient, restaurant, area and price. It has an
-          accurate system of reviews that, combined with the detailed
-          description of each product, gives the user the most complete dining
-          experience.
-        </Text>
       </View>
-    </View>
+    </SafeAreaView>
   );
+}
+}
+
+export default function FoodScreen({ navigation }) {
+  return <FoodScreenComponent navigation={navigation} />;
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1
+  backgroundContainer: {
+    flex: 1,
+    width: null,
+    height: null,
+    backgroundColor: "white",
+},
+logoText: {
+    position: "relative",
+    color: "black",
+    fontSize: 20,
+    paddingTop: 10,
+    paddingLeft:15,
+    paddingBottom: 25,
+    fontWeight: "bold",
+    opacity: 1,
+    textAlign: "left",
+},
+  subtitle: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "black",
+    marginLeft:5,
   },
-  rect2: {
-    top: 0,
-    left: 0,
-    width: 375,
-    height: 85,
-    position: "absolute",
-    backgroundColor: "rgba(252,149,126,1)"
-  },
-  rectStack: {
-    width: 375,
-    height: 85
-  },
-  aboutUs: {
-    top: 19,
-    left: 0,
-    position: "absolute",
-    fontFamily: "roboto-700",
-    color: "#121212",
-    fontSize: 28
-  },
-  rect3: {
-    top: 0,
-    left: 0,
-    width: 0,
-    height: 655,
-    position: "absolute",
-    backgroundColor: "#E6E6E6"
-  },
-  aboutUsStack: {
-    top: 0,
-    left: 1,
-    width: 112,
-    height: 655,
-    position: "absolute"
-  },
-  whoAreWe: {
-    top: 66,
-    left: 1,
-    position: "absolute",
-    fontFamily: "roboto-500",
-    color: "#121212"
+  text: {
+    marginLeft:5,
+    marginBottom:25,
   },
   button: {
-    top: 596,
-    left: 82,
-    width: 164,
-    height: 38,
-    position: "absolute",
-    backgroundColor: "rgba(252,149,126,1)",
-    borderRadius: 22
+    alignSelf:"center",
+    backgroundColor: "#FC987E",
+    borderRadius:5,
+    width: 100,
+    alignItems: "center",
+    paddingTop:8,
+    marginTop:100,
+    height: 40,
   },
   back: {
-    fontFamily: "roboto-regular",
-    color: "rgba(255,255,255,1)",
-    fontSize: 20,
-    marginTop: 7,
-    marginLeft: 56
-  },
-  weAre: {
-    top: 96,
-    left: 0,
-    position: "absolute",
-    fontFamily: "roboto-regular",
-    color: "#121212",
-    textAlign: "justify",
-    width: 308,
-    height: 51
-  },
-  whatIsFoodwayz: {
-    top: 185,
-    left: 2,
-    position: "absolute",
-    fontFamily: "roboto-regular",
-    color: "#121212"
-  },
-  weAre1: {
-    top: 202,
-    left: 2,
-    position: "absolute",
-    fontFamily: "roboto-regular",
-    color: "#121212",
-    textAlign: "justify",
-    width: 308,
-    height: 51
-  },
-  aboutUsStackStack: {
-    width: 310,
-    height: 655,
-    marginLeft: 22
+    color:"white",
   }
 });
-
-export default AboutUs;
