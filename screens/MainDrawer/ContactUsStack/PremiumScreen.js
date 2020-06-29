8562e25c48288a11888e6e38a071b54ecf930eac
CamilaPonce <cponce@itba.edu.ar>
2020-06-29 17:58:34 -0300

    tried premium

--- screens/MainDrawer/ContactUsStack/PremiumScreen.js
+++ screens/MainDrawer/ContactUsStack/PremiumScreen.js
@@ -74,8 +74,11 @@ class Premium extends Component {
                 <Text style={styles.text}>Month</Text>
                 <Picker      
                     selectedValue={this.state.expMonth}
-                    style={style.picker}
+                    style={styles.picker}
+                    mode = "dropdown"
+                    selectedValue = {1}
                     onValueChange={(itemValue, itemIndex) => this.setState({ expMonth: itemValue })}>
+                    
                     <Picker.Item label="1"  value={1} />
                     <Picker.Item label="2"  value={2} />
                     <Picker.Item label="3"  value={3} />
@@ -92,8 +95,11 @@ class Premium extends Component {
                 <Text style={styles.text}>Year</Text>
                 <Picker      
                     selectedValue={this.state.expYear}
-                    style={style.picker}
+                    style={styles.picker}
+                    mode = "dropdown"
+                    selectedValue = {2020}
                     onValueChange={(itemValue, itemIndex) => this.setState({ expYear: itemValue })}>
+                   
                     <Picker.Item label="2020"  value={2020} />
                     <Picker.Item label="2021"  value={2021} />
                     <Picker.Item label="2022"  value={2022} />
