0c272783614570222146cd6eb3053d2de96ada37
ncomerci <ncomerci@itba.edu.ar>
2020-06-30 14:31:02 -0300

    Update Owns.js

--- api/Owns.js
+++ api/Owns.js
@@ -16,8 +16,39 @@ class OwnsApi{
     return Api.baseUrl;
   }
 
+  /*
+  BODY:
+    --
+  RESULTADOS:
+    200 - se vinculó el owner id con restaurant id
+      EJEMPLO:
+      {
+        "message": "Successfully added owns",
+        "result": [
+          {
+            "a_user_id": 1,
+            "a_rest_id": 1
+          }
+        ]
+      }
+    409 - El usuario ya es owner
+      EJEMPLO:
+      {
+        "message": "Conflict with owner",
+        "description": [
+          {
+            "reason": "already exists",
+            "conflicting_obj": {
+              "a_user_id": 1,
+              "a_premium_level": 0
+            }
+          }
+        ]
+      }
+    500 - Falló la acción solicitada (probablemente no haya comunicación con la BD)
+  */
   static add(owns) {
-  return Api.post(`${OwnsApi.url}/owner/${owns.a_user_id}/restaurant/${owns.a_rest_id}`, owns);
+    return Api.post(`${OwnsApi.url}/owner/${owns.a_user_id}/restaurant/${owns.a_rest_id}`, owns);
   }
 
   static delete(id) {
