ade799a34870650c78af41349aff3c3d91866498
Matias S. Baiges <mbaiges@itba.edu.ar>
2020-07-06 10:54:58 -0300

    Update ContactUs.js

--- api/ContactUs.js
+++ api/ContactUs.js
@@ -14,15 +14,15 @@ class ContactUsApi{
   }
 
   static typeRequest(a_type_name) {
-    return Api.post(`${ContactUsApi.url}/contact_us/type_request`, {a_type_name});
+    return Api.post(`${ContactUsApi.url}/type_request`, {a_type_name});
   }
   
   static ingredientRequest(a_ingr_name) {
-    return Api.post(`${ContactUsApi.url}/contact_us/ingredient_request`, {a_ingr_name});
+    return Api.post(`${ContactUsApi.url}/ingredient_request`, {a_ingr_name});
   }
 
   static characteristicRequest(a_char_name) {
-    return Api.post(`${ContactUsApi.url}/contact_us/characteristic_request`, {a_char_name});
+    return Api.post(`${ContactUsApi.url}/characteristic_request`, {a_char_name});
   }
   
 }
